1316eb0050dedb55d10f1739c15f0e728f73b7af
camila yatabe <123522455+camiyuka@users.noreply.github.com>
2023-03-10 19:51:06 -0300

    pasta via upload

---
+++ projetoCor/app.js
@@ -0,0 +1,19 @@
+const colors=["#FFFFFF", "#FF0000", "#00FF00", " #0000FF","#FF00FF","#00FFFF","#FFFF00"
+,"#0000FF","#808080"];
+
+const btn= document.getElementById('btn');
+const color= document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    // get random number between 0 and 3 colors[3]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor= colors[randomNumber];
+    color.textContent= colors[randomNumber];
+});
+
+function getRandomNumber(){ 
+    return Math.floor(Math.random()*colors.length);
+
+}
